5b779f23c52ebd5c74d279ecb182d83c8a2c44a2
bus710 <bus710@gmail.com>
2023-08-30 11:59:40 -0700

    progress

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,16 +9,16 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  {
-    "tpope/vim-fugitive", cmd = "Git"
-  },
-  {
-    'simrat39/rust-tools.nvim'
-  },
-  {
-    'nvim-lua/plenary.nvim'
-  },
-  {
-    'mfussenegger/nvim-dap'
-  }
+  { "tpope/vim-fugitive", cmd = "Git" },
+  { "simrat39/rust-tools.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  { "mfussenegger/nvim-dap" },
+  { "othree/html5.vim" },
+  { "pangloss/vim-javascript" },
+  { "evanleck/vim-svelte", branch = "main" },
+  { "vim-erlang/vim-erlang-runtime" },
+  { "vim-erlang/vim-erlang-compiler" },
+  { "vim-erlang/vim-erlang-omnicomplete" },
+  { "vim-erlang/vim-erlang-tags" },
+  { "elixir-editors/vim-elixir" },
 }
