4a6dd74774e77352452b32bcb5a7318a784f1815
bus710 <bus710@gmail.com>
2023-08-30 12:08:52 -0700

    progress

--- plugins/user.lua
+++ plugins/user.lua
@@ -10,15 +10,47 @@ return {
   --   end,
   -- },
   { "tpope/vim-fugitive", cmd = "Git" },
-  { "simrat39/rust-tools.nvim" },
   { "nvim-lua/plenary.nvim" },
   { "mfussenegger/nvim-dap" },
+  --
   { "othree/html5.vim" },
   { "pangloss/vim-javascript" },
   { "evanleck/vim-svelte", branch = "main" },
+  --
+  { "simrat39/rust-tools.nvim" },
+  --
   { "vim-erlang/vim-erlang-runtime" },
   { "vim-erlang/vim-erlang-compiler" },
   { "vim-erlang/vim-erlang-omnicomplete" },
   { "vim-erlang/vim-erlang-tags" },
   { "elixir-editors/vim-elixir" },
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require "elixir"
+      local elixirls = require "elixir.elixirls"
+
+      elixir.setup {
+        nextls = { enable = true },
+        credo = {},
+        elixirls = {
+          enable = true,
+          settings = elixirls.settings {
+            dialyzerEnabled = false,
+            enableTestLenses = false,
+          },
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          end,
+        },
+      }
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
