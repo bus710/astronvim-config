dc155147a6481d403374a2dab873c2a3be772516
bus710 <bus710@gmail.com>
2023-08-30 17:14:30 -0700

    progress

--- plugins/user.lua
+++ plugins/user.lua
@@ -33,11 +33,10 @@ return {
       local elixirls = require "elixir.elixirls"
 
       elixir.setup {
-        nextls = { enable = true },
+        nextls = { enable = false },
         credo = { enable = true },
         elixirls = {
           enable = true,
-          -- cmd = "/home/$LOGNAME/.elixir-ls/release/language_server.sh",
           settings = elixirls.settings {
             dialyzerEnabled = false,
             enableTestLenses = false,
