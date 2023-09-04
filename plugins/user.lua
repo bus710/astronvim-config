71dbc753f24bc5c4fafb574c86f148124195e8a3
bus710 <bus710@gmail.com>
2023-09-03 19:20:54 -0700

    progress

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,6 +9,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- Command infer
+  { "folke/which-key.nvim" ,
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  },
+  --
+  -- Git
   { "tpope/vim-fugitive", cmd = "Git" },
   { 'rbong/vim-flog', cmd = "Flog" },
   -- 
