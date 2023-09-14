2deb31d1f126a95245a6dc01dd5b8b8684b578e2
bus710 <bus710@gmail.com>
2023-09-14 04:08:52 -0700

    progress

--- plugins/elixir-tools.lua
+++ plugins/elixir-tools.lua
@@ -20,7 +20,11 @@ return {
             enableTestLenses = false,
           },
           on_attach = function(client, bufnr)
-            -- vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover)
             vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
             vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
             vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
